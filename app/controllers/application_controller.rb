35ccb90f2fdfe1f3fda0f814a3e65865b09838ec
madeindjs <madeindjs@gmail.com>
2017-02-18 10:56:43 +0100

    create specific layout for restaurant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user_session, :current_user
 
+  before_action :found_restaurant
+
 
   private
   def current_user_session
@@ -16,6 +18,10 @@ class ApplicationController < ActionController::Base
 
   def check_login
     redirect_to home_path  unless current_user_session
+  end
+
+  def found_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id]) if params[:restaurant_id]
   end
 
   helper_method :current_user_session, :current_user
