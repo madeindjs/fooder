f83d97354c350ad4e5a92cd6b42c444340ed6e62
madeindjs <madeindjs@gmail.com>
2017-02-18 11:10:20 +0100

    add automatic catégories

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
   has_many :dishes
   has_many :categories
 
+  after_create :generate_categories
+
   def complete_name
     "#{self.lastname} #{self.firstname}"
+  end
+
+  private 
+
+  def generate_categories
+  	['Entrée', 'Plat', 'Dessrt', 'Boisson', 'Sauce'].each do |category_name|
+  		Category.create name: category_name, user_id: self.id
+  	end
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should create categories" do
+    assert_difference('Category.count', 5) do
+      User.create email: "test@test.fr", 
+        password: "20462046",
+        password_confirmation: "20462046",
+        firstname: "Test", 
+        lastname: "Test"
+    end
+  end
 end
