b702a949020989460aef64fd1d7c924a00a7586f
madeindjs <madeindjs@gmail.com>
2017-02-18 10:43:15 +0100

    add new reoutin for restaurant_dish_path and improve form

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
 
-  resources :categories
   resources :dishes
-  resources :restaurants
+
+  resources :restaurants do
+    resources :dishes
+  end
+
+
   resources :users
   resources :user_sessions, only: [:create, :destroy]
 
   get "home" => "pages#home"
 
-
   delete 'signout', to: 'user_sessions#destroy'
   get 'signin', to: 'user_sessions#new'
   get "signup" => "users#new"
+
+  scope '/restaurants/:id' do
+    
+  end
 
   root "pages#home"
 end
