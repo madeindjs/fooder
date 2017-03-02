4d6d03c6a19d65b92fc3eba64389b089550bc494
madeindjs <madeindjs@gmail.com>
2017-03-02 13:11:05 +0100

    display picture everywheres

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,15 +23,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  process resize_to_fit: [1000, 1000]
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
