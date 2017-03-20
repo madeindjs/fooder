d86e259b9a871cbd45bb4bc4a19a696a45cfa357
madeindjs <madeindjs@gmail.com>
2017-03-20 13:46:17 +0100

    add possibility to load a custom favicon

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -30,6 +30,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [200, 200]
   end
 
+  version :favicon do
+    process resize_to_fill: [32, 32]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
