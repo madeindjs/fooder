f0e6a622876e1ae7aab8c65c3642b07fc30e1474
madeindjs <madeindjs@gmail.com>
2017-03-30 13:21:53 +0200

    add months to payements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330110952) do
+ActiveRecord::Schema.define(version: 20170330112053) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170330110952) do
     t.integer  "status"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "months"
   end
 
   create_table "posts", force: :cascade do |t|
