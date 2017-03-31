e6d58fef3a0617a34ae00f6dea19a5a13dce9185
madeindjs <madeindjs@gmail.com>
2017-03-31 13:10:10 +0200

    get prices from model

--- app/models/payement.rb
+++ app/models/payement.rb
@@ -1,13 +1,4 @@
 class Payement < ApplicationRecord
-  PRICES = [
-    ['1 mois (9,99€)', 9.99],
-    ['3 mois (26,99€)', 26.99],
-    ['6 mois (55,99€)', 55.99],
-    ['1 an (99,99€)', 99.99],
-    ['2 ans (199,99€)', 199.99],
-    ['A vie (259.99€)', 259.99],
-  ]
-
 
   def paypal_url
     values = {
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+
+  def self.options_for_select
+    ActionController::Base.helpers.options_for_select Product.all.map{ |p| [ "#{p.name} ( #{p.price} €)" , p.price] }
+  end
+
 end
