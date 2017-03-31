3a0294cff324abb6c91b649c645539786b81c2e5
madeindjs <madeindjs@gmail.com>
2017-03-31 12:45:48 +0200

    Create form to buy a premium account

--- app/models/payement.rb
+++ app/models/payement.rb
@@ -1,2 +1,19 @@
 class Payement < ApplicationRecord
+  PRICES = [
+    ['1 mois (9,99€)', 9.99],
+    ['3 mois (26,99€)', 26.99],
+    ['6 mois (55,99€)', 55.99],
+    ['1 an (99,99€)', 99.99],
+    ['2 ans (199,99€)', 199.99],
+    ['A vie (259.99€)', 259.99],
+  ]
+
+
+  def paypal_url
+    values = {
+      cmd: "_s-xclick",
+      hosted_button_id: "2PQCPQ32JNP6A",
+      amount: 20,
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,20 @@ class User < ApplicationRecord
     false
   end
 
+  def paypal_url(return_path)
+    values = {
+        business: "merchant@gotealeaf.com",
+        cmd: "_xclick",
+        upload: 1,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
+        invoice: id,
+        amount: course.price,
+        item_name: course.name,
+        item_number: course.id,
+        quantity: '1'
+    }
+    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+
+
 end
