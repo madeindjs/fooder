0a4d6e86d52d092bee982348e7dbde1215e9043c
madeindjs <madeindjs@gmail.com>
2017-04-18 13:37:24 +0200

    setup mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def activate_account(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Bienvenue sur Fooder, activez votre compte dès à présent')
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def activate_account
+    UserMailer.activate_account User.new email: "alex@alex.fr", firstname: "Rousseau", lastname: 'Alexandre'
+  end
+
 end
