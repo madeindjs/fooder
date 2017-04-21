2434167f432410f1f465e1623870327ab74eb9a6
madeindjs <madeindjs@gmail.com>
2017-04-21 13:39:40 +0200

    create views for restaurants mailer

--- test/mailers/previews/restaurant_mailer_preview.rb
+++ test/mailers/previews/restaurant_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/restaurant_mailer
 class RestaurantMailerPreview < ActionMailer::Preview
 
+  def contact
+    RestaurantMailer.contact( {
+      'object'  => 'Hello', 
+      'email'   => 'customer@free.fr', 
+      'content' => 'Lorem ipsum Ut ex ex labore nisi in consequat non officia ea dolore aliquip officia Excepteur aliqua dolor voluptate deserunt pariatur eu tempor aute minim nisi.'
+    }, 'restaurant@free.fr')
+  end
+
 end
