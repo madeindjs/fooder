c8a8c3c06a727ba08aa36aeeeab4cc9299170aba
madeindjs <madeindjs@gmail.com>
2017-04-27 13:56:51 +0200

    repair unit tests

--- test/controllers/newsletters_controller_test.rb
+++ test/controllers/newsletters_controller_test.rb
@@ -5,44 +5,21 @@ class NewslettersControllerTest < ActionDispatch::IntegrationTest
     @newsletter = newsletters(:one)
   end
 
-  test "should get index" do
-    get newsletters_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_newsletter_url
-    assert_response :success
-  end
-
+  
   test "should create newsletter" do
     assert_difference('Newsletter.count') do
-      post newsletters_url, params: { newsletter: {  } }
+      post newsletters_url, params: { newsletter: { email: 'madeindjs@gmail.com' } }
     end
 
-    assert_redirected_to newsletter_url(Newsletter.last)
+    assert_redirected_to root_url
   end
 
-  test "should show newsletter" do
-    get newsletter_url(@newsletter)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_newsletter_url(@newsletter)
-    assert_response :success
-  end
-
-  test "should update newsletter" do
-    patch newsletter_url(@newsletter), params: { newsletter: {  } }
-    assert_redirected_to newsletter_url(@newsletter)
-  end
 
   test "should destroy newsletter" do
     assert_difference('Newsletter.count', -1) do
       delete newsletter_url(@newsletter)
     end
 
-    assert_redirected_to newsletters_url
+    assert_redirected_to root_url
   end
 end
