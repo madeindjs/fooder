6989b73347eb9f64cfc851324eb9771d36057bf4
madeindjs <madeindjs@gmail.com>
2017-05-03 12:56:23 +0200

    add informations to newsletter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427110716) do
+ActiveRecord::Schema.define(version: 20170503105520) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20170427110716) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "website"
   end
 
   create_table "opening_hours", force: :cascade do |t|
