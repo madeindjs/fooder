809368a12c2448274754baaaebedd7304790bc53
Alexandre Rousseau <madeindjs@gmail.com>
2017-05-20 17:21:59 +0200

    add new input to link form

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -97,7 +97,7 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:name, :url)
+      params.require(:link).permit(:name, :url, :link_type_id)
     end
 
     def check_owner
