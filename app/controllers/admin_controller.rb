badfcc593cfd81f8c3dbd35a2c4f41f00675ed91
Alexandre Rousseau <madeindjs@gmail.com>
2017-05-21 16:04:05 +0200

    create administration system

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,24 @@
 class AdminController < ApplicationController
   before_action :check_login
   before_action :check_admin
+
+  def index
+    @data = {
+      "Produits" => {
+        color: 'primary',
+        glypicon: 'apple',
+        objects: @restaurant.dishes.select{ |d| d.activate },
+      },
+      "Menus" => {
+        color: 'danger',
+        glypicon: 'cutlery',
+        objects: @restaurant.menus.select{ |d| d.activate },
+      },
+      "Articles" => {
+        color: 'warning',
+        glypicon: 'comment',
+        objects: @restaurant.posts,
+      },
+    }
+  end
 end
