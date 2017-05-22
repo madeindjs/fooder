4c44ed6da590bf5725253279a717282c36e10f0c
madeindjs <madeindjs@gmail.com>
2017-05-22 13:07:54 +0200

    add sidebar

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,17 @@ class AdminController < ApplicationController
   before_action :check_login
   before_action :check_admin
 
+  before_action :set_sidebar_data
+
   layout 'admin'
 
   def index
-    @data = {
+  end
+
+  private
+
+  def set_sidebar_data
+    @sidebar_data = {
       "Produits" => {
         color: 'primary',
         glyphicon: 'apple',
@@ -18,14 +25,14 @@ class AdminController < ApplicationController
         objects: @restaurant.menus.select{ |d| d.activate },
         link: menus_edit_path,
         module: 'menus',
-      },
-      "Articles" => {
-        color: 'warning',
-        glyphicon: 'comment',
-        objects: @restaurant.posts,
-        link: posts_path,
-        module: 'blog',
-      },
-    }.select{|item, data| data[:module] == nil || @restaurant.send("module_"+data[:module])  }
+        },
+        "Articles" => {
+          color: 'warning',
+          glyphicon: 'comment',
+          objects: @restaurant.posts,
+          link: posts_path,
+          module: 'blog',
+          },
+        }.select{|item, data| data[:module] == nil || @restaurant.send("module_"+data[:module])  }
   end
 end
