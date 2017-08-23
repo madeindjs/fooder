68ba9de46ed234d74b4d0a36c87f38715752e598
madeindjs <madeindjs@gmail.com>
2017-08-23 14:03:41 +0200

    desactivate capistrano sitemap

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 namespace :deploy do
 
   after :updated, :bundle_install do
-    invoke "sitemap:refresh"
+    # invoke "sitemap:refresh"
     # invoke "ruby_on_rails:bundle_install"
     # invoke "ruby_on_rails:db_migrate"
     # invoke "ruby_on_rails:assets_precompile"
