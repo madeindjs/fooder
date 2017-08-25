d5c5ea9c9b159b1c71db10452a31f26a14f908c7
madeindjs <madeindjs@gmail.com>
2017-08-25 13:44:37 +0200

    Create commercial mailer

--- app/mailers/restaurant_mailer.rb
+++ app/mailers/restaurant_mailer.rb
@@ -7,4 +7,10 @@ class RestaurantMailer < ApplicationMailer
     mail(to: restaurant_mail, subject: @object)
   end
 
+  def commercial restaurant
+    @object  = 'Bienvenue sur Fooder'
+    @restaurant  = restaurant
+    mail(to: restaurant.email, subject: @object)
+  end
+
 end
