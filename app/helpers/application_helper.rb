2c61b2921a2f6b62c4825029996aa826a1a47ded
madeindjs <madeindjs@gmail.com>
2017-09-07 14:35:08 +0200

    Improve global design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def glyphicon name
-      "<span class=\"glyphicon glyphicon-#{name}\" aria-hidden=\"true\"></span>".html_safe
+    "<span class=\"glyphicon glyphicon-#{name}\" aria-hidden=\"true\"></span>".html_safe
   end
 
 
@@ -12,7 +12,7 @@ module ApplicationHelper
 
   def bootstrap_label color, content
     "<span class=\"label label-#{color}\">#{content}</span>".html_safe
-  end 
+  end
 
 
   def tags tags
@@ -39,23 +39,30 @@ module ApplicationHelper
   end
 
   # Create a picture tag with option
-  def picture_tag model, cutsom_field, css_class: "", size: 50
-    unless model.picture.thumb.file.nil?
-      option_selected = ImageFormat::DISPLAY[ @restaurant.send cutsom_field ]
+  def picture_tag model, cutsom_field, css_class: "", size: nil
+    # build options
+    option_selected = ImageFormat::DISPLAY[ @restaurant.send cutsom_field ]
+    options = {
+      alt: "photo du #{model}",
+      class: "img-responsive #{option_selected.values.first} #{css_class}"
+    }
+    options['size'] = size if size
 
-      image_tag model.picture.thumb.url, 
-          class: "#{option_selected.values.first} #{css_class}", 
-          alt: "photo du #{model}", 
-          size: size
+    if model.picture.thumb.file.nil?
+      # todo: replace this url
+      url = "https://recipes.heart.org/-/media/aha/recipe/course-category-icons/side-dishes.svg"
+    else
+      url = model.picture.thumb.url
     end
+    image_tag url, options
   end
 
 
   def tooltip content
     if admin?
-     html = 'title="' + h(content) + '"'
-     html.html_safe
-   end
+      html = 'title="' + h(content) + '"'
+      html.html_safe
+    end
   end
 
 
