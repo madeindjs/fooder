0a478d919b8357561339784bfc1e1d394cf517e8
madeindjs <madeindjs@gmail.com>
2017-10-02 13:38:59 +0200

    Setup AJAX for sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -9,18 +9,15 @@ class SectionsController < ApplicationController
 
 
   # GET /sections/new
-  def new
-    @title = "Nouveau contenu"
-    @description = "Ajouter du contenu à la page d'accueil de ce restaurant."
-    @section = Section.new
-  end
+  # def new
+  #   @title = "Nouveau contenu"
+  #   @description = "Ajouter du contenu à la page d'accueil de ce restaurant."
+  #   @section = Section.new
+  # end
 
   # GET /sections/1/edit
   def edit
-    @title = "Editer le contenu."
-    @description = "Editer le contenu de la page d'accueil."
-
-    render  '_form', locals: {section: @section}, layout:  false if request.xhr?
+    render  '_form', locals: {section: @section}, layout:  false
   end
 
 
@@ -42,11 +39,9 @@ class SectionsController < ApplicationController
   # PATCH/PUT /sections/1
   def update
     if @section.update(section_params)
-      flash[:success] = "Votre texte d'accueil a été supprimé."
-      redirect_to root_url
+      render  'sections/_list', locals: {sections: @restaurant.sections}, layout: false
     else
-      flash[:danger] = "Une erreur est survenue."
-      render :edit
+      render plain: "Une erreur est survenue."
     end
   end
 
@@ -58,17 +53,17 @@ class SectionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_section
-      @section = Section.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_section
+    @section = Section.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def section_params
-      params.require(:section).permit(:title, :content, :picture)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def section_params
+    params.require(:section).permit(:title, :content, :picture)
+  end
 
-    def check_owner
-      redirect_to root_path unless current_user.sections.include? @section
-    end
+  def check_owner
+    redirect_to root_path unless current_user.sections.include? @section
+  end
 end
