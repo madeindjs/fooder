a160dde10079789b04feb6efaf297587f0f742c0
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-10-08 18:19:37 +0200

    repair remove link for dishes & menus

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,16 +1,10 @@
 // dish has been updated / created
-$(document).on('ajax:success', 'form.new_menu, form.edit_menu', function(e, data, status, xhr){
+$(document).on('ajax:success', 'form.new_menu, form.edit_menu, form.delete_menu', function(e, data, status, xhr){
   $('#menus-list').html(xhr.responseText);
   $('.ui-dialog').remove();
 });
 
-$(document).on('ajax:error', 'form.new_menu, form.edit_menu', function(e, xhr, status, error){
+$(document).on('ajax:error', 'form.new_menu, form.edit_menu, form.delete_menu', function(e, xhr, status, error){
   var form = $(this);
   form.parent().html(xhr.responseText);
-});
-
-
-$(document).on('ajax:success', 'form.new_menu, form.edit_menu', function(e, data, status, xhr){
-  $('#menus-list').html(xhr.responseText);
-  $('.ui-dialog').remove();
 });
