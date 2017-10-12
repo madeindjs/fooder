a13f2718f26e2eb8ba4f8bc4ffcdb2d6427b04bd
madeindjs <madeindjs@gmail.com>
2017-10-12 12:51:35 +0200

    Add AJAX picture upload

--- app/assets/javascripts/sections.js
+++ app/assets/javascripts/sections.js
@@ -4,7 +4,16 @@ $(document).on('ajax:success', 'form.new_section, form.edit_section, form.delete
   $('.dialog').dialog('close');
 });
 
-$(document).on('ajax:error', 'form.new_section, form.edit_section, form.delete_section', function(e, xhr, status, error){
-  var form = $(this);
-  form.parent().html(xhr.responseText);
+$(document).on("ajax:remotipartComplete", 'form.new_section, form.edit_section, form.delete_section', function(e, xhr){
+  $('#sections-list').html(xhr.responseText);
+  $('.dialog').dialog('close');
 });
+
+
+/**
+ * `ajax:error` is called after `ajax:remotipartComplete` due to [remotipart](https://github.com/JangoSteve/remotipart) gem
+ */
+// $(document).on('ajax:error', 'form.new_section, form.edit_section, form.delete_section', function(e, xhr, status, error){
+//   var form = $(this);
+//   form.parent().html(xhr.responseText);
+// });
