2ed4b7a82191ff09cef597537c70c21e0050b50f
madeindjs <madeindjs@gmail.com>
2017-10-13 12:53:14 +0200

    add AJAX edit && remove link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,7 @@ class Link < ApplicationRecord
   belongs_to :restaurant
   mount_uploader :logo, PictureUploader
 
+  validates_presence_of :name
+  validates_presence_of :url
   validates_presence_of :restaurant_id
 end
